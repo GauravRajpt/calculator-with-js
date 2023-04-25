f6b1f6d9f424c39a8029e7a4c56da92983acb1e1
GauravRajpt <joygrajput@gmail.com>
2023-04-25 09:17:01 +0530

    fixing bug

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-let str="";
+let str=""
+let input= document.getElementById('input');
+input.value=str;
+        
 const buttonList= document.getElementsByTagName("button");
 console.log(buttonList)
 Array.from(buttonList).forEach((element)=>{
     element.addEventListener('click',()=>{
-        if(element.innerHTML== "="){
+        if(element.innerHTML=== "="){
             console.log(str)
-            input.value=eval(str);
+            input.value=eval(input.value);
         }
-        else if(element.innerHTML=="c"){
+        else if(element.innerHTML==="c"){
             input.value="";
         }
         else{
-        str= str+ element.innerHTML;
-        let input= document.getElementById('input');
-input.value= str;
+        input.value=input.value+ element.innerHTML;
         }
     })
 })
